b25e9dd0c6d5687fad30348f2acbd2fd7a3837d0
Charcoaljam1 <atompaul25@gmail.com>
2025-03-16 21:25:56 +0000

    Updated database connection script

--- scripts/database_connection.py
+++ scripts/database_connection.py
@@ -1,10 +1,10 @@
 import psycopg2
-from config import db_config
+from config import DB_CONFIG
 
 def get_db_connection():
     """Creates and returns a PostgreSQL database connection."""
     try:
-        conn = psycopg2.connect(**db_config)
+        conn = psycopg2.connect(**DB_CONFIG)
         return conn
     except Exception as e:
         print(f'Error creating connection: {e}')
